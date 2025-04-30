6469f6bef8de4b87adecae3fb167ca56ecd11ac4
Pjaxon999 <180225649+Pjaxon999@users.noreply.github.com>
2025-04-30 14:17:51 -0600

    add append, prepend, size methods
    Probably should have committed these one at a time, oh well! append adds
    a node to the end of the list, prepend adds it to the start of the list,
    and size returns the total size of the list. I added some testing stuff
    to the index.js file so that when I run it I can see if my methods are
    working correctly. (TDD lite?)

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 import LinkedList from "./linkedList.js";
 console.log(LinkedList.listName);
+const list = new LinkedList();
+console.log(list.size()); // "This list is empty! Disgraceful!"
+list.append(1);
+console.log(list.size()); // Should return 1
+list.append(2);
+list.append(3);
+console.log(list.size()); // Should return 3
--- linkedList.js
+++ linkedList.js
@@ -1,8 +1,41 @@
 import Node from "./node.js";
 export default class LinkedList {
-    constructor(head, tail) {
-        this.head = head;
-        this.tail = tail;
+    constructor() {
+        this.head = null;
+        this.tail = null;
+    }
+
+    append(value) {
+        const newNode = new Node(value);
+        if (this.head === null) {
+            this.head = newNode;
+        } else {
+            this.tail.nextNode = newNode;
+        }
+        this.tail = newNode;
+    }
+
+    prepend(value) {
+        const newNode = new Node(value);
+        newNode.nextNode = this.head;
+        this.head = newNode;
+        if (this.tail === null) {
+            this.tail = newNode;
+        }
+    }
+
+    size() {
+        // starting from the head, follow the pointers to the tail and increment the count each time
+        let count = 0;
+        if (this.head === null) return "This list is empty! Disgraceful!";
+        else {
+            let currentNode = this.head;
+            while (currentNode !== null) {
+                count++;
+                currentNode = currentNode.nextNode;
+            }
+        }
+        return count;
     }
 
     static listName = "I am a linked list abstraction";
